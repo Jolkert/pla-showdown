94f501f27ea8e0d53806d1a40550015987c96253
jolkert <jolkertt@gmail.com>
2025-04-12 03:57:17 -0400

    Made `SerType` fields pub

--- src/data/serialization/types.rs
+++ src/data/serialization/types.rs
@@ -5,12 +5,12 @@ use crate::{BoxSlice, BoxStr};
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct SerType
 {
-	id: Rc<str>,
+	pub id: Rc<str>,
 
 	#[serde(rename = "weaknesses")]
-	weakness_ids: BoxSlice<BoxStr>,
+	pub weakness_ids: BoxSlice<BoxStr>,
 	#[serde(rename = "resistances")]
-	resistance_ids: BoxSlice<BoxStr>,
+	pub resistance_ids: BoxSlice<BoxStr>,
 	#[serde(rename = "immunities")]
-	immunity_ids: BoxSlice<BoxStr>,
+	pub immunity_ids: BoxSlice<BoxStr>,
 }
