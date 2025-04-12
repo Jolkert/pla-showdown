7d635e91f3b282eea13a20ad0ab2f8b562fb96c7
jolkert <jolkertt@gmail.com>
2025-04-12 17:48:04 -0400

    `main.rs` refactored for new data models

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,81 @@
-//use lazy_static::lazy_static;
-//use pla_showdown::data::{
-//	Identifiable, Move, Nature, RegMap, Species, StatusCondition, Type,
-//	serialization::{SerMove, SerSpecies, SerStatus},
-//};
-//
-//lazy_static! {
-//	static ref TYPE_MAP: RegMap<Type> = register("assets/types/");
-//	static ref MOVE_MAP: RegMap<Move<'static>> = register::<SerMove>("assets/moves/")
-//		.iter()
-//		.map(|it| (it.0.to_owned(), it.1.clone().into_move(&TYPE_MAP)))
-//		.collect();
-//	static ref SPECIES_MAP: RegMap<Species<'static>> = register::<SerSpecies>("assets/species/")
-//		.iter()
-//		.map(|it| (it.0.to_owned(), it.1.clone().into_species(&TYPE_MAP)))
-//		.collect();
-//	static ref STATUS_MAP: RegMap<StatusCondition<'static>> =
-//		register::<SerStatus>("assets/statuses/")
-//			.iter()
-//			.map(|it| (it.0.to_owned(), it.1.clone().into_status(&TYPE_MAP)))
-//			.collect();
-//	static ref NATURE_MAP: RegMap<Nature> =
-//		toml::from_str(&std::fs::read_to_string("assets/natures.toml").unwrap()).unwrap();
-//}
-//
+use std::{
+	collections::{HashMap, HashSet},
+	path::Path,
+};
+
+use pla_showdown::data::{
+	IdSet, Nature,
+	serialization::{SerMove, SerSpecies, SerStatus, SerType},
+};
+
 fn main()
 {
-	println!("WIP");
+	// TODO: make the `into_*` conversion mapping generic
+	// - morgan 2025-04-12
+
+	let types = {
+		let deser_types = deserialize_dir::<SerType>("./assets/types").collect::<Vec<_>>();
+
+		let ids = deser_types
+			.iter()
+			.map(|ty| ty.id.clone())
+			.collect::<HashSet<_>>();
+
+		deser_types
+			.into_iter()
+			.map(|ser| ser.into_type(&ids).into())
+			.collect::<IdSet<_>>()
+	};
+
+	let species = deserialize_dir::<SerSpecies>("./assets/species")
+		.map(|ser| ser.into_species(&types).into())
+		.collect::<IdSet<_>>();
+
+	let moves = deserialize_dir::<SerMove>("./assets/moves")
+		.map(|ser| ser.into_move(&types).into())
+		.collect::<IdSet<_>>();
+
+	let statuses = deserialize_dir::<SerStatus>("./assets/statuses")
+		.map(|ser| ser.into_status(&types).into())
+		.collect::<IdSet<_>>();
+
+	let natures = toml::from_str::<HashMap<Box<str>, Nature>>(
+		&std::fs::read_to_string("./assets/natures.toml").unwrap(),
+	)
+	.unwrap();
+
+	println!("Types: {}", types.len());
+	println!("Species: {}", species.len());
+	println!("Moves: {}", moves.len());
+	println!("Statuses: {}", statuses.len());
+	println!("Natures: {}", natures.len());
 }
-//fn main()
-//{
-//	println!("Types: {}", TYPE_MAP.len());
-//	println!("Species: {}", SPECIES_MAP.len());
-//	println!("Moves: {}", MOVE_MAP.len());
-//	println!("Statuses: {}", STATUS_MAP.len());
-//	println!("Natures: {}", NATURE_MAP.len());
-//}
-//
-//fn register<T>(dir_path: &str) -> RegMap<T>
-//where
-//	T: serde::de::DeserializeOwned + Identifiable,
-//{
-//	std::fs::read_dir(dir_path)
-//		.unwrap_or_else(|_| panic!("directory '{dir_path}' not found!"))
-//		.filter_map(|result| {
-//			result.ok().and_then(|file| {
-//				std::fs::read_to_string(file.path()).ok().and_then(|data| {
-//					toml::from_str::<T>(&data)
-//						.inspect_err(|err| {
-//							eprintln!("Failed to deserialize file {:#?}: {err}", file.file_name());
-//						})
-//						.ok()
-//						.map(|t| (t.id(), t))
-//				})
-//			})
-//		})
-//		.collect()
-//}
+
+fn deserialize_dir<T: serde::de::DeserializeOwned>(
+	path: impl AsRef<Path>,
+) -> impl Iterator<Item = T>
+{
+	std::fs::read_dir(&path)
+		.unwrap_or_else(|err| {
+			panic!(
+				"Could find directory `{}`!\n{err}",
+				path.as_ref().to_string_lossy()
+			)
+		})
+		.filter_map(|result| {
+			result.ok().and_then(|file| {
+				std::fs::read_to_string(file.path())
+					.ok()
+					.and_then(|toml_str| {
+						toml::from_str::<T>(&toml_str)
+							.inspect_err(|err| {
+								eprintln!(
+									"Failed to deserialize file {:#?}\n{err}",
+									file.file_name()
+								)
+							})
+							.ok()
+					})
+			})
+		})
+}
