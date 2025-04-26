fb3a0b0b1a7d054709271614a027e7eaa9e4425f
jolkert <jolkertt@gmail.com>
2025-04-26 18:29:20 -0400

    split `deserialization` in bin

---
+++ bin/src/deserialize.rs
@@ -0,0 +1,82 @@
+use std::{
+	collections::{HashMap, HashSet},
+	path::Path,
+};
+
+use pla_showdown::data::{
+	Data, IdSet, Identify, Nature,
+	serialization::{IntoDeserialized, SerMove, SerSpecies, SerStatus, SerType},
+};
+
+pub fn initialize_data() -> Data
+{
+	log::info!("Loading types...");
+	let types = {
+		let deser_types = deserialize_dir::<SerType>("./assets/types").collect::<Vec<_>>();
+		let ids = deser_types
+			.iter()
+			.map(|ty| ty.id.clone())
+			.collect::<HashSet<_>>();
+
+		id_set_from(deser_types, &ids)
+	};
+
+	log::info!("Loading species...");
+	let species = id_set_from(deserialize_dir::<SerSpecies>("./assets/species"), &types);
+
+	log::info!("Loading moves...");
+	let moves = id_set_from(deserialize_dir::<SerMove>("./assets/moves"), &types);
+
+	log::info!("Loading statuses...");
+	let statuses = id_set_from(deserialize_dir::<SerStatus>("./assets/statuses"), &types);
+
+	Data {
+		types,
+		species,
+		moves,
+		statuses,
+		natures: toml::from_str::<HashMap<Box<str>, Nature>>(
+			&std::fs::read_to_string("./assets/natures.toml").unwrap(),
+		)
+		.unwrap(),
+	}
+}
+
+fn deserialize_dir<T: serde::de::DeserializeOwned>(
+	path: impl AsRef<Path>,
+) -> impl Iterator<Item = T>
+{
+	std::fs::read_dir(&path)
+		.unwrap_or_else(|err| {
+			panic!(
+				"Could find directory `{}`!\n{err}",
+				path.as_ref().to_string_lossy()
+			)
+		})
+		.filter_map(|result| {
+			result.ok().and_then(|file| {
+				std::fs::read_to_string(file.path())
+					.ok()
+					.and_then(|toml_str| {
+						toml::from_str::<T>(&toml_str)
+							.inspect_err(|err| {
+								log::error!(
+									"Failed to deserialize file {}\n{err}",
+									file.file_name().display()
+								);
+							})
+							.ok()
+					})
+			})
+		})
+}
+
+fn id_set_from<'a, S, D, R>(iter: impl IntoIterator<Item = S>, data: &'a R) -> IdSet<D>
+where
+	S: IntoDeserialized<'a, Deserialized = D, RefData = R>,
+	D: Identify,
+{
+	iter.into_iter()
+		.map(|item| item.into_deserialized(data))
+		.collect()
+}
--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,15 +1,10 @@
-use std::{
-	collections::{HashMap, HashSet},
-	path::Path,
-	rc::Rc,
-};
+use std::rc::Rc;
 
 use eframe::egui::{self, Widget};
-use pla_showdown::data::{
-	Data, IdSet, Identify, Move, Nature, NatureEffect, Pokemon, Species, Stat, Type,
-	serialization::{IntoDeserialized, SerMove, SerSpecies, SerStatus, SerType},
-};
+use pla_showdown::data::{Data, Identify, Move, NatureEffect, Pokemon, Species, Stat, Type};
 use strum::VariantArray;
+
+mod deserialize;
 
 fn main()
 {
@@ -24,7 +19,7 @@ fn main()
 	let _ = eframe::run_native(
 		"pla-showdown",
 		options,
-		Box::new(|_| Ok(Box::from(Showdown::new(initialize_data())))),
+		Box::new(|_| Ok(Box::from(Showdown::new(deserialize::initialize_data())))),
 	);
 }
 
@@ -303,77 +298,4 @@ impl Showdown
 			})
 		});
 	}
-}
-
-fn initialize_data() -> Data
-{
-	log::info!("Loading types...");
-	let types = {
-		let deser_types = deserialize_dir::<SerType>("./assets/types").collect::<Vec<_>>();
-		let ids = deser_types
-			.iter()
-			.map(|ty| ty.id.clone())
-			.collect::<HashSet<_>>();
-
-		id_set_from(deser_types, &ids)
-	};
-
-	log::info!("Loading species...");
-	let species = id_set_from(deserialize_dir::<SerSpecies>("./assets/species"), &types);
-
-	log::info!("Loading moves...");
-	let moves = id_set_from(deserialize_dir::<SerMove>("./assets/moves"), &types);
-
-	log::info!("Loading statuses...");
-	let statuses = id_set_from(deserialize_dir::<SerStatus>("./assets/statuses"), &types);
-
-	Data {
-		types,
-		species,
-		moves,
-		statuses,
-		natures: toml::from_str::<HashMap<Box<str>, Nature>>(
-			&std::fs::read_to_string("./assets/natures.toml").unwrap(),
-		)
-		.unwrap(),
-	}
-}
-
-fn deserialize_dir<T: serde::de::DeserializeOwned>(
-	path: impl AsRef<Path>,
-) -> impl Iterator<Item = T>
-{
-	std::fs::read_dir(&path)
-		.unwrap_or_else(|err| {
-			panic!(
-				"Could find directory `{}`!\n{err}",
-				path.as_ref().to_string_lossy()
-			)
-		})
-		.filter_map(|result| {
-			result.ok().and_then(|file| {
-				std::fs::read_to_string(file.path())
-					.ok()
-					.and_then(|toml_str| {
-						toml::from_str::<T>(&toml_str)
-							.inspect_err(|err| {
-								log::error!(
-									"Failed to deserialize file {}\n{err}",
-									file.file_name().display()
-								);
-							})
-							.ok()
-					})
-			})
-		})
-}
-
-fn id_set_from<'a, S, D, R>(iter: impl IntoIterator<Item = S>, data: &'a R) -> IdSet<D>
-where
-	S: IntoDeserialized<'a, Deserialized = D, RefData = R>,
-	D: Identify,
-{
-	iter.into_iter()
-		.map(|item| item.into_deserialized(data))
-		.collect()
 }
